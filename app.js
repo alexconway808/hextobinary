d7fcdb122e1d319bca6c4ad82d478ff95d98b2db
Alex Conway <alexconway808@gmail.com>
2014-08-14 19:21:16 -1000

    It works in the console

--- app.js
+++ app.js
@@ -1,43 +1,41 @@
-// //Hex to Decimal
-// function ConvertToDec(){
-//   var hexNumber = document.getElementById('NumberInput').value;
-//   //parse the hex value
-//   var decNumber = parseInt(hexNumber,16);
-//   console.log(decNumber)
-//   document.getElementById('Result').value = decNumber;
+
+  //Input handler
+  var user_input = document.getElementById("user_input");
+  var button = document.getElementById("find");
+
+  button.onclick = function (){
+    var splittheinputs = user_input.value.trim().split(" ");
+    // console.log(splittheinputs);
+    for (k = 0; k < splittheinputs.length; k++){
+      // console.log(splittheinputs[k]);
+      whatdahex(splittheinputs[k]);
+    }
+  }
 
 
-// //Decimal to Binary
-//  function ConvertToBinary(){
-//   var numberValue = document.getElementById('NumberInput').value;
-//   var decNumber = Number(numberValue);
-//   var binaryNumber = decNumber.toString(2).toUpperCase();
+  //Processing input
+function whatdahex (string){
+  var hextoDecimal = parseInt(string, 16);
+  var decimalToBinary = hextoDecimal.toString(2)
+  var decimalArray = decimalToBinary.split("");
+  // console.log(decimalArray);
+  var numberOfZerosToAdd = 8 - decimalArray.length;
+  var concatArray = [];
+  for (i = 0; i < numberOfZerosToAdd; i++){
+    concatArray.push("0");
+  //Combine the two arrays
+  var combinedArray = concatArray.concat(decimalArray);
+  // console.log(combinedArray);
 
-//Input handler
-
-
-//Validate the inputs are Hex
-
-
-//Processing input
-var hexInput = "7E";
-var hextoDecimal = parseInt(hexInput, 16);
-var decimalToBinary = hextoDecimal.toString(2)
-var decimalArray = decimalToBinary.split("");
-var numberOfZerosToAdd = 8 - decimalArray.length;
-var concatArray = [];
-for (i = 0; i < numberOfZerosToAdd; i++){
-  concatArray.push(0);
+  //Convert the array into X's
+  for (j = 0; j < combinedArray.length; j++){
+    if(combinedArray[j] === "1"){
+      combinedArray[j] = "X";
+    }
+  }
+  };
+  console.log(combinedArray);
 };
 
-//Combine the two arrays
-var combinedArray = concatArray.concat(decimalArray);
-console.log(combinedArray);
 
-//Convert the array into X's
-for (j = 0; j < combinedArray.length; j++){
-  if(combinedArray[j] === "1"){
-    combinedArray[j] = "X";
-  }
-}
-console.log(combinedArray);
+// })();
